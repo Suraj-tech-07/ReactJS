39cdd9c23fdb6aa32af7b94339f9b176928ff6f6
Surya <suryask7549@gmail.com>
2024-08-11 19:35:52 +0530

    Learning React 11-08-2024 Day 2 Changes 2

--- React_Project_by_Components_1/src/main.jsx
+++ React_Project_by_Components_1/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import TodoContainer from './TodoContainer'
-import './main.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App.jsx'
+import './index.css'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TodoContainer></TodoContainer>
+    <App />
   </StrictMode>,
 )
