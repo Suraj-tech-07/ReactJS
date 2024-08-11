4b3811271511714970289f8756c33a4e6291b0cd
Surya <suryask7549@gmail.com>
2024-08-11 19:13:05 +0530

    Learning React 11-08-2024 Day 2

--- React_4_Bootstrap/React_4_Bootstrap/src/main.jsx
+++ React_4_Bootstrap/React_4_Bootstrap/src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Component from './ReusableComponent'
+import App from './App.jsx'
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<Component></Component>
-{/* <Component></Component>
-<Component></Component>
-<Component></Component>
-<Component></Component>
-<Component></Component> */}
+    <App />
   </React.StrictMode>,
 )
