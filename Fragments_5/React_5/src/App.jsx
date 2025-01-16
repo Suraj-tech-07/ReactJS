83aa3a2d8d1e39fa48be5a51dd1aaa6cef07f8b9
Surya <suryask7549@gmail.com>
2025-01-16 23:21:43 +0530

    React Fragments

---
+++ Fragments_5/React_5/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return// This is React Fragment <>....</>
+  <>
+    <h1>Subjcts Lists for Sem 1 : </h1>
+    <ul className="list-group">
+      <li className="list-group-item w-75  ">Problem Solving Using C</li>
+      <li className="list-group-item w-75 ">Fundamental of Computer Science and Emerging Technologies</li>
+      <li className="list-group-item w-75 ">Computer Organization and Architecture</li>
+      <li className="list-group-item w-75 ">Principles of Management and Communications</li>
+      <li className="list-group-item w-75 ">Descrete Mathematics</li>
+      <li className="list-group-item w-75 ">Cyber Security</li>
+    </ul>
+  </>
+}
+export default App;
