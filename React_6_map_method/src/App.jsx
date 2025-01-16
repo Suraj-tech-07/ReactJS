73b29e359624603796849c4ff1909199c48e565c
Surya <suryask7549@gmail.com>
2025-01-16 23:59:36 +0530

    Conditional rendering and map method in React

--- React_6_map_method/src/App.jsx
+++ React_6_map_method/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  let subjects = ["Descrete Mathematics", "Problem Solving Using C", "Principles of Management and Communication", "Fundamental of Computer Science and Emerging Technologies", "Computer Organization and Architecture"];
+  // let subjects = [];
+  // if (subjects.length === 0) {
+  //   return <>
+  //     <h2>
+  //       Nothing in the list
+  //     </h2>
+  //   </>
+  // }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <>
+    <h1>Subjcts : </h1>
+    {subjects.length === 0 ? <h3>No Items Available</h3> : null}
+    <ul className="list-group">
+      {
+        subjects.map((items) => (
+          <li className="list-group-item" key={items}> {items}</li>
+        ))
+      }
+    </ul>
+  </>
 }
 
-export default App
+export default App;
