2407131b0c104f217ac99895c6390d9ee6f1d00e
Surya <suryask7549@gmail.com>
2025-02-08 23:59:17 +0530

    Full Functioning TODO App Updated

--- Projects/Full_Functioning_TODO_APP/src/App.jsx
+++ Projects/Full_Functioning_TODO_APP/src/App.jsx
@@ -8,19 +8,28 @@ import { useState } from 'react';
 const App = () => {
   const todoTitle = "Daily Todo App";
   const noItem = "There is Nothing todo";
-  // const Items = [
-  //   { name: "Buy Books", date: "10/12/2025" },
-  //   { name: "Buy Clothes", date: "10/12/2026" },
-  //   { name: "Buy Merchandise", date: "10/12/2027" },
-  // ];
+  const Items = [
+    { name: "Buy Books", date: "10/12/2025" },
+    { name: "Buy Clothes", date: "10/12/2026" },
+    { name: "Buy Merchandise", date: "10/12/2027" },
+  ];
 
-  const [item, setItem] = useState();
+  const [todoItems, setTodoItems] = useState(Items);
+
   // const [itemDate, setItemDate] = useState();
+  // const [itemName, setItemName] = useState();
+
+  const onNewItem = (itemName, itemDate) => {
+    console.log(`New Items are : ${itemName} Date : ${itemDate}`);
+    setTodoItems({ name: itemName, date: itemDate });
+    console.log(todoItems);
+
+  }
 
   return <>
     <Container className={styles.appContainer}>
       <Title title={todoTitle}></Title>
-      <AddNewItem></AddNewItem>
+      <AddNewItem onNewItem={onNewItem}></AddNewItem>
       {
         Items.length === 0 && <NoItemMessage noItem={noItem}></NoItemMessage>
       }
--- Projects/Full_Functioning_TODO_APP/src/components/TodoAddNewItem.jsx
+++ Projects/Full_Functioning_TODO_APP/src/components/TodoAddNewItem.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import styles from './AddNewItem.module.css';
-const AddNewItem = () => {
+const AddNewItem = ({ onNewItem }) => {
+    const [name, setName] = useState("");
+    const [date, setDate] = useState("");
 
+    const onNameChange = (event) => {
+        setName(event.target.value);
+    }
+    const onDateChange = (event) => {
+        setDate(event.target.value);
+    }
+
+    const handleOnClick = () => {
+        onNewItem(name, date)
+        setName("");
+        setDate("");
+    }
     return <>
         <div className={styles.addContainer}>
-            <input type="text" name="todoname" id="todoname" className="" />
-            <input type="date" name="tododate" id="tododate" className="" />
-            <button className="btn btn-success">Add</button>
+            <input type="text" name="todoname" id="todoname" value={name} className="" onChange={onNameChange} />
+            <input type="date" name="tododate" id="tododate" value={date} className="" onChange={onDateChange} />
+            <button className="btn btn-success" onClick={handleOnClick}>Add</button>
         </div>
     </>
 }
