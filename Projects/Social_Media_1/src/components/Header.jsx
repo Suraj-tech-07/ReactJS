a9a84bc4dd23fbe15e48630fcc46cfa38ea0bea5
Surya <suryask7549@gmail.com>
2025-03-15 20:15:34 +0530

    refactor Header component and add CSS module for styling

--- Projects/Social_Media_1/src/components/Header.jsx
+++ Projects/Social_Media_1/src/components/Header.jsx
@@ -1,18 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { GiHamburgerMenu } from "react-icons/gi";
+import styles from "../css/Header.module.css";
 import logo from "../assets/logo.jpg";
 const Header = () => {
-    return
-    <div className="header">
-        <div className="header-hamburger disabled">
-            <GiHamburgerMenu />
-        </div>
-        <div className="headerlogo">
-            <img src={logo} alt="logo" height="40px" width="40px" className='rounded-circle' />
-        </div>
+    return (
+        <div className={`${styles.header}`} >
+            <div className={`${styles.header_hamburger, styles.disabled} `}>
+                <GiHamburgerMenu />
+            </div>
 
-        <div className="header-navbar">navbar </div>
-        <div className="header-login-signup">login and signup</div>
-    </div>
+            <div className={`${styles.header_logo}`}>
+                <img src={logo} alt="logo" height="40px" width="40px" className='rounded-circle' />
+            </div>
+
+            <div className="header-navbar">
+                <ul className='list-group list-group-horizontal space-between'>
+                    <li>Home </li>
+                    <li>Products</li>
+                    <li>About</li>
+                    <li>Services</li>
+                </ul>
+            </div>
+            <div className="header-login-signup">login and signup</div>
+        </div >
+    );
 }
 export default Header;
