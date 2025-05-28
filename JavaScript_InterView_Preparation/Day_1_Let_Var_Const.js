47575f36a06e6b3d9c9735b637c555023e3c2835
Surya <suryask7549@gmail.com>
2025-05-28 22:55:06 +0530

    JavaScript Day 1

---
+++ JavaScript_InterView_Preparation/Day_1_Let_Var_Const.js
@@ -0,0 +1,36 @@
+// Let and Const : Blocked Scope
+// Var : Global Scope
+
+
+// {
+//     let a = 10;
+// }
+// console.log(a);
+
+
+// let a = 30;
+// {
+//     let a = 10;
+//     console.log(a);
+
+// }
+// console.log(a);
+
+
+
+// const a = 10;
+// {
+
+//     a = 30;
+// }
+// console.log(a);
+
+
+let hi="Hii";
+{
+    let hi="Hello";
+}
+console.log(hi);
+
+
+
